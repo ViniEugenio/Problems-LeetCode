8878e67ef410aba9313b0b90796c398040d94dc0
Vinicius Eugenio <vinicius_eugenio07@hotmail.com>
2025-01-21 14:41:40 -0300

    83

--- 89 - Remove Duplicates from Sorted List/Program.cs
+++ 89 - Remove Duplicates from Sorted List/Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+static ListNode DeleteDuplicates(ListNode head)
+{
+
+    if (head == null)
+    {
+        return head;
+    }
+
+    while (head.next != null && head.val == head.next.val)
+    {
+        head.next = head.next.next;
+    }
+
+    DeleteDuplicates(head.next);
+    return head;
+
+}
+
+static void MostrarNumeros(ListNode? head)
+{
+
+    if (head?.val == null)
+    {
+        return;
+    }
+
+    Console.WriteLine(head.val);
+    MostrarNumeros(head.next);
+
+}
+
+var quintoNo = new ListNode(3, null);
+var quartoNo = new ListNode(3, quintoNo);
+var terceiroNo = new ListNode(2, quartoNo);
+var segundoNo = new ListNode(1, terceiroNo);
+var primeiroNo = new ListNode(1, segundoNo);
+
+Console.WriteLine("Números de entrada:");
+MostrarNumeros(primeiroNo);
+
+var numerosRemovendoDuplicacoes = DeleteDuplicates(primeiroNo);
+
+Console.WriteLine();
+Console.WriteLine("Números removendo duplicações:");
+MostrarNumeros(numerosRemovendoDuplicacoes);
+
+public class ListNode
+{
+
+    public int val;
+    public ListNode? next;
+
+    public ListNode(int val = 0, ListNode? next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+
+}
+
--- ClimbingStairs/Program.cs
+++ ClimbingStairs/Program.cs
@@ -1,9 +1,6 @@
-﻿using ClimbingStairs;
-
-
-var testes = new int[]
+﻿var testes = new int[]
 {
-   3
+   4
 };
 
 for (int x = 0; x < testes.Length; x++)
@@ -15,5 +12,26 @@ for (int x = 0; x < testes.Length; x++)
     Console.WriteLine($"{testes[x]} = {resultado}");
 
     Console.WriteLine();
+
+}
+
+public static class ClimberManager
+{
+
+    private static Dictionary<int, int> resultadosAnteriores = new Dictionary<int, int>();
+
+    public static int ClimbStairs(int n)
+    {
+        if (n <= 1)
+        {
+            resultadosAnteriores.Add(n - 1, 1);
+            return 1;
+        }
+
+        int primeiroValor = ClimbStairs(n - 1);
+        resultadosAnteriores.Add(n - 1, primeiroValor);
+
+        return primeiroValor + resultadosAnteriores[n - 2];
+    }
 
 }
